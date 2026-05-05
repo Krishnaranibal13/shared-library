67174db8e40b25a4bb8409d7a05add879b8d357b
Krishnaranibal13 <144999063+Krishnaranibal13@users.noreply.github.com>
2026-05-05 13:13:21 +0530

    Delete update_k8s_manifests.groovy

--- update_k8s_manifests.groovy
+++
@@ -1,7 +0,0 @@
-def call(Map config) {
-    echo "Updating Kubernetes manifests with image tag: ${config.imageTag}"
-
-    sh """
-    sed -i 's|image:.*|image: ${env.DOCKER_IMAGE_NAME}:${config.imageTag}|g' ${config.manifestsPath}/deployment.yaml
-    """
-}
